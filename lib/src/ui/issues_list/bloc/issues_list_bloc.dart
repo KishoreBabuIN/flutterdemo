a460265ab182ec65ba2793e48d839596f69b8e34
Kishore Babu <barykaed@gmail.com>
2022-01-15 02:05:37 +0530

    cleanup

--- lib/src/ui/issues_list/bloc/issues_list_bloc.dart
+++ lib/src/ui/issues_list/bloc/issues_list_bloc.dart
@@ -1,22 +1,8 @@
-import 'package:bloc_concurrency/bloc_concurrency.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_demo/src/data/github_issues_repository.dart';
 import 'package:flutter_demo/src/network/model/issue.dart';
 import 'package:flutter_demo/src/ui/issues_list/bloc/issues_list_event.dart';
 import 'package:flutter_demo/src/ui/issues_list/bloc/issues_list_state.dart';
-import 'package:stream_transform/stream_transform.dart';
-
-// const _issuesPerPage = 30;
-const _throttleDuration = Duration(milliseconds: 100);
-int _currentPage = 1;
-String _ownerName = "flutter";
-String _repoName = "flutter";
-
-EventTransformer<E> throttleDroppable<E>(Duration duration) {
-  return (events, mapper) {
-    return droppable<E>().call(events.throttle(duration), mapper);
-  };
-}
 
 class IssuesListBloc extends Bloc<IssuesListEvent, IssuesListState> {
   IssuesListBloc({
@@ -27,13 +13,15 @@ class IssuesListBloc extends Bloc<IssuesListEvent, IssuesListState> {
         fetchFirstPage: (e) => _onFetch(emit, e),
         fetchNextPage: (e) => _fetchNextPage(emit, e),
       ),
-      transformer: throttleDroppable(_throttleDuration),
     );
   }
+  final String _ownerName = "flutter";
+  final String _repoName = "flutter";
   final GithubIssuesRepository repository;
   final List<Issue> _issues = [];
   bool _hasReachedEnd = false;
   bool _isLoadingNextPage = false;
+  int _currentPage = 1;
 
   Future<void> _onFetch(Emitter<IssuesListState> emit, e) async {
     try {
